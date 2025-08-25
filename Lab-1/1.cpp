7f265a2da56ce258c66773fc02a6ee263432cd47
janshuthourani123-boop <janshuthourani123@gmail.com>
2025-08-25 19:17:21 +0500

    1.cpp

--- Lab-1/1.cpp
+++ Lab-1/1.cpp
@@ -1,32 +1,27 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-class BankAccount {
-public:
-    float bal;
-    
-    BankAccount() {
-        bal = 0.0;
+using namespace std;
+class BankAccount{
+    public:
+    float balance;
+    BankAccount(){
+        balance = 0.0;
     }
-    
-    BankAccount(float b) {
-        bal = b;
+    BankAccount(float b){
+       balance = b;
     }
-    
-    void Display() {
-        cout << "Balance: K$" << bal << endl;
+    void Display(){
+        cout<<"Balance: "<<balance<<endl;
     }
 };
-
 int main() {
-    BankAccount account1;
-    account1.Display();
-    
-    BankAccount account2(100.0f);
-    account2.Display();
-    
-    BankAccount account3(account2);
-    account3.Display();
-    
+  BankAccount account1;
+  account1.Display();
+  BankAccount account2(1000);
+  account2.Display();
+  BankAccount account3 = account2;
+  account3.Display();
+  account3 = account3.balance - 200.0;
+  account3.Display();  
     return 0;
 }
